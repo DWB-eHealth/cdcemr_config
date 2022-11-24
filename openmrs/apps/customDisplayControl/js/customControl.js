bb9a5b67d5d81cf585299d7cc0c028f6e4e6ef81
Thomas RODRIGUEZ <trodriguezmsf@users.noreply.github.com>
2022-11-24 10:41:17 +0100

    appointmentsv2 - clinical dashboard display

--- openmrs/apps/customDisplayControl/js/customControl.js
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -205,7 +205,7 @@ angular.module('bahmni.common.displaycontrol.custom')
         });
 
         $scope.goToListView = function () {
-            $window.open('/bahmni/appointments/#/home/manage/appointments/list');
+            $window.open('/appointments-v2/#/home/manage/appointments/list');
         };
         $scope.openJitsiMeet = function (appointmentIndex) {
             var uuid = $scope.upcomingAppointmentsUUIDs[appointmentIndex];
